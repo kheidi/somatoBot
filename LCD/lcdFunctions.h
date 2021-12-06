2ff2f0904832d5261a8724668c5b8bca48eaa05e
Katherine Fehr <katherine.heidi@gmail.com>
2021-12-06 12:26:05 -0600

    Initializes LCD functions

---
+++ LCD/lcdFunctions.h
@@ -0,0 +1,11 @@
+/*
+* Author: K. Heidi Fehr (kfehr@wisc.edu)
+* Github: @kheidi
+* Filename: lcdFunctions.h
+* Description: description
+* Created:  2021-12-06T01:18:31.693Z
+* (c)Copyright 2021
+*/
+
+
+
