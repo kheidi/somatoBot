32a0c266f186d168c26741e2784a8e127da97a63
Katherine Fehr <kfehr@wisc.edu>
2021-12-15 20:34:25 -0600

    LCD functions ready

--- LCD/lcdFunctions.h
+++ LCD/lcdFunctions.h
@@ -16,7 +16,21 @@
 #include <delay.h>
 
 #define LCDdelay 10  //us Pick a nice delay time to ramp up motor speed
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+#define PINK 3
+#define CHARTREUSE 4
+#define WHITE 5
+#define PURPLE 6
+#define YELLOW 7
 
+
+void clearLCD(void);
+void printStringLCD(char *ch);
+void makePink(void);
+void setLCDColor(char colorMacro);
+void setLCDbrightness(char value);
 
 #endif
 
