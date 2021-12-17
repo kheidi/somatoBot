2fe368b2261dcfe8d14327d5ff2210ec6269f109
sbhhernandez <hernandezher@wisc.edu>
2021-12-17 09:35:37 -0600

    LCD things commented out

--- testLCD/main.c
+++ testLCD/main.c
@@ -137,8 +137,14 @@ PCICR=(0<<PCIE2) | (0<<PCIE1) | (0<<PCIE0);
 // USART Transmitter: On
 // USART Mode: Asynchronous
 // USART Baud Rate: 9600
+// UCSR0A=(0<<RXC0) | (0<<TXC0) | (0<<UDRE0) | (0<<FE0) | (0<<DOR0) | (0<<UPE0) | (0<<U2X0) | (0<<MPCM0);
+// UCSR0B=(0<<RXCIE0) | (0<<TXCIE0) | (0<<UDRIE0) | (0<<RXEN0) | (1<<TXEN0) | (0<<UCSZ02) | (0<<RXB80) | (0<<TXB80);
+// UCSR0C=(0<<UMSEL01) | (0<<UMSEL00) | (0<<UPM01) | (0<<UPM00) | (0<<USBS0) | (1<<UCSZ01) | (1<<UCSZ00) | (0<<UCPOL0);
+// UBRR0H=0x00;
+// UBRR0L=0x67;
+
 UCSR0A=(0<<RXC0) | (0<<TXC0) | (0<<UDRE0) | (0<<FE0) | (0<<DOR0) | (0<<UPE0) | (0<<U2X0) | (0<<MPCM0);
-UCSR0B=(0<<RXCIE0) | (0<<TXCIE0) | (0<<UDRIE0) | (0<<RXEN0) | (1<<TXEN0) | (0<<UCSZ02) | (0<<RXB80) | (0<<TXB80);
+UCSR0B=(0<<RXCIE0) | (0<<TXCIE0) | (0<<UDRIE0) | (1<<RXEN0) | (1<<TXEN0) | (0<<UCSZ02) | (0<<RXB80) | (0<<TXB80);
 UCSR0C=(0<<UMSEL01) | (0<<UMSEL00) | (0<<UPM01) | (0<<UPM00) | (0<<USBS0) | (1<<UCSZ01) | (1<<UCSZ00) | (0<<UCPOL0);
 UBRR0H=0x00;
 UBRR0L=0x67;
@@ -167,22 +173,45 @@ SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<
 // TWI disabled
 TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
 
-clearLCD();
-
-printStringLCD("Rainbow Cycle!");
-for( i = 0; i < 8; i++ ){
-      displayColor(i);
-	  delay_ms(500);
-	  if (i == 7)
-	  {
-		  i=0;
-	  }
-   }
+// stopMotors();
+// clearLCD();
+// setLCDbrightness(100);
+// 
+// putchar(0xFE);
+// putchar(0xD1);
+// putchar(16);
+// putchar(2);
+// delay_ms(LCDdelay);
+// 
+// putchar(0xFE);
+// putchar(0xD1);
+// putchar(255);
+// delay_ms(LCDdelay);
+ putchar('H');
+ putchar('H');
+ putchar('H');
+ putchar('H');
+putchar('H');
+putchar('H');
+putchar('H');
+putchar('H');
+putchar('H');
+putchar('H');
+ puts("Hello");
+//printStringLCD("Rainbow Cycle!");
+// for( i = 0; i < 8; i++ ){
+//       setLCDColor(i);
+// 	  delay_ms(500);
+// 	  if (i == 7)
+// 	  {
+// 		  i=0;
+// 	  }
+//    }
 
 
 while (1)
       {
       // Place your code here
-
+	stopMotors();
       }
 }
